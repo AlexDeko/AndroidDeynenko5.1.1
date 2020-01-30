f2386dd6dc8898759807ea85916dc853fe249769
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-01-30 22:51:12 +0300

    reWrite

--- app/src/main/java/com/homework1_3/androiddeynenko431/MainActivity.java
+++ app/src/main/java/com/homework1_3/androiddeynenko431/MainActivity.java
@@ -5,13 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,10 +16,17 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -35,17 +36,14 @@ public class MainActivity extends AppCompatActivity {
     private final static String LOG_TAG = "sample";
     private final static String TITLE = "title";
     private final static String SUBTITLE = "subtitle";
-    private final static String TEXT = "text";
-    private final static String PREF = "pref";
     List<Map<String, String>> simpleAdapterContent;
-    private SharedPreferences sharedPref;
     private ListView list;
     private String result;
     private String[] content;
     File fileExample;
     FileReader exReader;
     FileWriter exWriter;
-    Map<String, String> row;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,24 +52,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Можноли вместо getApplicationContext() , писать this
-        try {
-            fileExample = new File(getApplicationContext().getExternalFilesDir(null), "anEx.docx");
-            if(!fileExample.mkdirs()){
-                Log.e(LOG_TAG,"Directory not created");
-            }
-            exWriter = new FileWriter(fileExample);
-            exWriter.append(getString(R.string.large_text));
 
-            exReader = new FileReader(fileExample);
-             result = exReader.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.e(LOG_TAG,"Error");
+        fileExample = new File(getExternalFilesDir(null), "anEx.docx");
+        if (fileExample.getParentFile() != null && !fileExample.getParentFile().mkdirs()) {
+            Log.e(LOG_TAG, "Directory not created");
         }
 
+        if (!fileExample.exists()) {
+            try {
+                fileExample.createNewFile();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "File not created");
+            }
+        }
 
-      //  Bitmap bitmap = BitmapFactory.decodeFile(fileExample.getAbsolutePath());
+        // Закрываем ресурс с помощью try with resources
+        try (BufferedWriter exWriter = new BufferedWriter(new OutputStreamWriter(new
+                FileOutputStream(fileExample)))) {
+            exWriter.append(getString(R.string.large_text));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(LOG_TAG, "Error");
+        }
+        readFile();
 
         content = prepareContent();
         final BaseAdapter listContentAdapter = createAdapter(content);
@@ -81,9 +84,8 @@ public class MainActivity extends AppCompatActivity {
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
-                //final View item = (TextView) parent.getItemAtPosition(position);
-
+            public void onItemClick(AdapterView<?> parent, final View view, final int position,
+                                    long id) {
                 ImageButton imageButtonDelete = findViewById(R.id.btnDelete);
                 View.OnClickListener deleteBtn = new View.OnClickListener() {
                     @Override
@@ -92,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
                                 .withEndAction(new Runnable() {
                                     @Override
                                     public void run() {
-                                        // list.removeView(item);
                                         simpleAdapterContent.remove(position);
                                         listContentAdapter.notifyDataSetChanged();
                                         view.setAlpha(1);
@@ -102,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 };
                 imageButtonDelete.setOnClickListener(deleteBtn);
 
-                Button btnAdd =findViewById(R.id.btnAdd);
+                Button btnAdd = findViewById(R.id.btnAdd);
                 btnAdd.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -112,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
+                        Map<String, String> row = new HashMap<>();
                         row.put(TITLE, newString.getText().toString());
                         row.put(SUBTITLE, String.valueOf(newString.length()));
                         simpleAdapterContent.add(row);
@@ -132,8 +134,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void readFile() {
+        try (BufferedReader exReader = new BufferedReader(new InputStreamReader(new
+                FileInputStream(fileExample)))) {
+            String line;
+            StringBuilder resultBuilder = new StringBuilder();
+            while ((line = exReader.readLine()) != null) {
+                resultBuilder.append(line);
+                resultBuilder.append("\n");
+            }
+            result = resultBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void updateList() {
-        result = exReader.toString();
+        readFile();
         content = prepareContent();
     }
 
